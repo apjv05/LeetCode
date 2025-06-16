563d474dce56e94d9a88ff1a119d5ce75b9c709c
Andrew <andrewpvoyez@gmail.com>
2025-06-15 20:32:54 -0500

    Create 0735_AsteroidCollision.cpp

---
+++ Problems/Stack/0735_AsteroidCollision.cpp
@@ -0,0 +1,22 @@
+vector<int> asteroidCollision(vector<int>& asteroids) {
+    vector<int> stack;
+
+    for (int& a : asteroids) {
+        while (!stack.empty() && a < 0 && stack.back() > 0) {
+
+            //equation to figure out which asteroid wins in a collision
+            int diff = a + stack.back();
+            
+            if (diff < 0) stack.pop_back();
+            else if (diff > 0) a = 0;
+            else {
+                a = 0;
+                stack.pop_back();
+            }
+        }
+        if (a != 0) {
+            stack.push_back(a);
+        }
+    }
+    return stack;
+}
