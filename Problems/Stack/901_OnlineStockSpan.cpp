d570e1172643487a2a923b2782aad739cebe4369
Andrew <andrewpvoyez@gmail.com>
2025-07-01 13:56:42 -0500

    Update 901_OnlineStockSpan.cpp

--- Problems/Stack/901_OnlineStockSpan.cpp
+++ Problems/Stack/901_OnlineStockSpan.cpp
@@ -8,12 +8,15 @@ public:
 
         //while there is still previous days && our current price is > all the previous prices
         while(!st.empty() && st.top().first <= price) {
-            //increment our span and pop the stack to move on to the previous day
+            //add the span of the previous day and pop it so that way we know that any other 
+            //days we have visted before are all less than it
+            //than pop the previous number because we have a new greatest number
             span += st.top().second;
             st.pop();
         }
 
-        //push the found day on to the stack and return the span for the output
+        //push the found day on to the stack so we can look at it later for future reference
+        //and return the span for the output
         st.push({price, span});
         return span;
     }
