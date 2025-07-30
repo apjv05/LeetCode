a13452afe39de18ddd422770ea1da5b8947bd62d
apjv05 <andrewpvoyez@gmail.com>
2025-07-30 14:24:26 -0500

    Create 0100_SameTree.cpp

---
+++ Problems/Trees/0100_SameTree.cpp
@@ -0,0 +1,8 @@
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    if(p == nullptr && q == nullptr) return true;
+
+    if(p != nullptr && q != nullptr && p->val == q->val) {
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+    return false;    
+}
